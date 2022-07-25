1cdab0328e55a2d8ab3cc4f2942ddee6f89f6ade
Stewart Hector <stewart@stewarthector.com>
2022-07-24 22:42:15 -0400

    Add additional test

--- monitor/route/host.py
+++ monitor/route/host.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 
 from monitor import schema, exceptions, service
 from monitor.service import create_host_service
@@ -12,8 +12,8 @@ router = APIRouter(
 
 @router.get("/{host_id}", response_model=schema.Host)
 def get_host(
-        host_id: str,
-        host_service: service.HostService = Depends(service.create_host_service),
+    host_id: str,
+    host_service: service.HostService = Depends(service.create_host_service),
 ):
     host = host_service.get_one(host_id)
 
@@ -25,13 +25,18 @@ def get_host(
 
 @router.get("", response_model=list[schema.Host], status_code=HTTPStatus.OK)
 def get_hosts(host_service: service.HostService = Depends(service.create_host_service)):
-    hosts = host_service.get_all()
+    try:
+        hosts = host_service.get_all()
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail={"message": str(e)}
+        )
     return hosts
 
 
 @router.post("", status_code=HTTPStatus.CREATED)
 def create_host(
-        host_create: schema.HostCreate,
-        host_service: service.HostService = Depends(create_host_service),
+    host_create: schema.HostCreate,
+    host_service: service.HostService = Depends(create_host_service),
 ):
     host_service.create_host(host_create)
--- test/unit/routes/test_host.py
+++ test/unit/routes/test_host.py
@@ -18,8 +18,9 @@ class TestHost(BaseRouteTest):
     def host_data(self):
         hosts = [
             models.Host(
-                **{'id': 1, 'name': 'Raspberry Pi', 'ip_address': '192.168.0.1', 'enabled': True}),
-            models.Host(**{'id': 2, 'name': 'Apple', 'ip_address': '192.168.0.2', 'enabled': True}),
+                **{"id": 1, "name": "Raspberry Pi", "ip_address": "192.168.0.1", "enabled": True}
+            ),
+            models.Host(**{"id": 2, "name": "Apple", "ip_address": "192.168.0.2", "enabled": True}),
         ]
 
         return hosts
@@ -27,18 +28,24 @@ class TestHost(BaseRouteTest):
     @pytest.fixture(autouse=True)
     def setup_test(self):
         self.mock_host_service = create_autospec(HostService)
-        mock_create_host_service = create_autospec(create_host_service,
-                                                   return_value=self.mock_host_service)
+        mock_create_host_service = create_autospec(
+            create_host_service, return_value=self.mock_host_service
+        )
 
         app.dependency_overrides[service.create_host_service] = mock_create_host_service
 
         yield
 
-    def test_get_host(self, test_client, host_data):
+    def test_get_host_and_return_all_hosts(self, test_client, host_data):
         self.mock_host_service.get_all.return_value = host_data
 
-        response = test_client.get('/api/v1/host')
+        response = test_client.get("/api/v1/host")
         assert response.status_code == HTTPStatus.OK
 
         expected = model_list_to_json(host_data)
         assert response.json() == expected
+
+    def test_get_host_and_expect_500_exception(self, test_client):
+        self.mock_host_service.get_all.side_effect = Exception("test exception")
+        response = test_client.get("/api/v1/host")
+        assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
